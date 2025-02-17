7f4dc95d5711efa6be36788e546f94e8902789d0
karthick-கார்த்திக் <a.karthie+git@pm.me>
2025-02-17 09:18:53 +0000

    remove and refactor (#9)

--- src/client/client.rs
+++ src/client/client.rs
@@ -20,19 +20,40 @@ impl<'a> Client<'a> {
         TcpStream::connect(self.base_url).expect("error connecting to server to write")
     }
 
+    pub fn http_req_from_path(&self, method: &str, path: &str) -> String {
+        let mut base = self.base_url.to_string();
+        if path.starts_with("/") {
+            base.push_str(path);
+            return self.build_http_req(method, &base);
+        }
+        path.to_string().insert_str(0, "/");
+        base.push_str(path);
+        self.build_http_req(method, &base)
+    }
+
+    fn get_path(&self, url: &Uri) -> Result<String, &'static str> {
+        if url.path() == "" {
+            let e = Err("invalid path supplied");
+            return e;
+        }
+        match url.path_and_query() {
+            Some(pq) => return Ok(pq.to_string()),
+            _ => return Err("invalid query param supplied"),
+        }
+    }
+
     pub fn build_http_req(&self, method: &str, url: &str) -> String {
         let uri: Uri = url.parse().expect("invalid url supplied");
+        let path_query = self
+            .get_path(&uri)
+            .expect("invalid query param and path supplied");
         let hostname = uri.host().expect("invalid host provided");
         let port = uri.port().expect("invalid port supplied");
         let host_name = [hostname, port.as_str()].join(":");
-        let mut path = uri.path();
-        if path == "" {
-            path = "/"
-        }
         let mut req = String::new();
         req.push_str(method.to_uppercase().as_str());
         req.push_str(" ");
-        req.push_str(path);
+        req.push_str(path_query.as_str());
         req.push_str(" ");
         req.push_str("HTTP/1.1\r\n");
         req.push_str("Host: ");
@@ -61,5 +82,35 @@ mod test {
         let result = client.build_http_req("GET", "http://localhost:8888/");
         let want = "GET / HTTP/1.1\r\nHost: localhost:8888\r\nAccept: */*\r\n\r\n";
         assert_eq!(result, want.to_string());
+    }
+    #[test]
+    fn build_req_from_path() {
+        let mut client = Client::default();
+        let addr = "http://localhost:8080";
+        client.address(&addr);
+        let result = client.http_req_from_path("GET", "/");
+        let want = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nAccept: */*\r\n\r\n";
+        assert_eq!(result, want)
+    }
+    #[test]
+    fn url_with_no_slash() {
+        let addr = "home";
+        let mut client = Client::default();
+        client.address(&addr);
+        let url: Uri = addr.parse().unwrap();
+        let v = match client.get_path(&url) {
+            Ok(v) => v,
+            Err(e) => e.to_string(),
+        };
+        println!("v:{v}");
+        assert_ne!(v, addr);
+    }
+
+    #[test]
+    fn connect_stream() {
+        let addr = "http://localhost:8888";
+        let mut client = Client::default();
+        client.address(&addr);
+        client.connect_stream();
     }
 }
